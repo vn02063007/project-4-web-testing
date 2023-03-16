f245367f0c2c79b7e088f30316203c466239db5a
vn02063007 <113915639+vn02063007@users.noreply.github.com>
2023-03-16 11:29:13 +0800

    Update recommend_code.py

--- recommend_code.py
+++ recommend_code.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sb
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
